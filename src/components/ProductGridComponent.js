c29d587190b8ee69bfc2a5b6989be2dbce66c49c
jmaguero <juanmaguero@hotmail.com>
2022-10-19 14:15:10 +0200

    Font and alignment corrections

--- src/components/ProductGridComponent.js
+++ src/components/ProductGridComponent.js
@@ -5,9 +5,9 @@ export const ProductGridComponent = ({ id, imgUrl, name, description }) => {
   return (
     <NavLink className="m-2 rounded-md border-2 border-light-green text-dark-green bg-light-green" style={{ padding: "1.2em" }} to={`/product/${id}`}>
       <img className="p-2" src={imgUrl} alt={name} width="320px" />
-      <h2 className="p-2 text-2xl font-bold">{name}</h2>
-      <p className="p-2 text-lg font-semibold">Description:</p>
-      <p className="p-2">{description}</p>
+      <h2 className="p-2 text-2xl font-extrabold">{name}</h2>
+      <p className="p-2 text-lg font-extrabold">Description:</p>
+      <p className="p-2 font-bold">{description}</p>
     </NavLink>
   )
 }
