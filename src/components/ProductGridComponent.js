78747c2243d80daf5104223bfe9c99b6500452d4
jmaguero <juanmaguero@hotmail.com>
2022-10-19 13:44:44 +0200

    Product grid styled

--- src/components/ProductGridComponent.js
+++ src/components/ProductGridComponent.js
@@ -3,10 +3,11 @@ import { NavLink } from 'react-router-dom'
 
 export const ProductGridComponent = ({ id, imgUrl, name, description }) => {
   return (
-    <NavLink style={{ padding: "1.2em" }} to={`/product/${id}`}>
-      <img src={imgUrl} alt={name} width="320px" />
-      <h2>{name}</h2>
-      <p>{description}</p>
+    <NavLink className="m-2 rounded-md border-2 border-light-green text-dark-green bg-light-green" style={{ padding: "1.2em" }} to={`/product/${id}`}>
+      <img className="p-2" src={imgUrl} alt={name} width="320px" />
+      <h2 className="p-2 text-2xl font-bold">{name}</h2>
+      <p className="p-2 text-lg font-semibold">Description:</p>
+      <p className="p-2">{description}</p>
     </NavLink>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,16 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  colors: {
-    'light-green': '#E3EECD',
-    'med-green': '#ACD1B6',
-    'dark-green': '#408E61',
-    'light-blue': '#71A596',
-    'dark-blue': '#40676E'
-  },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-green': '#E3EECD',
+        'med-green': '#ACD1B6',
+        'dark-green': '#408E61',
+        'light-blue': '#71A596',
+        'dark-blue': '#40676E'
+      },
+    },
   },
-  plugins: [],
+  plugins: []
 }
